3680efa3972b3e6862d93e46bcb188c498b5d980
Hugolifes <rach.rc36@gmail.com>
2020-09-06 21:09:23 -0600

    Listo :3

--- lib/src/screens/feed.dart
+++ lib/src/screens/feed.dart
@@ -1,38 +1,65 @@
+import 'package:coivd_app2/model/product.dart';
+import 'package:coivd_app2/model/products_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Feed extends StatelessWidget {
   @override //sobreescribe la clase
 
-  List<Card> _buildGridCards(int count) {
-    List<Card> cards = List.generate(
-      count,
-      (int index) => Card(
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
         clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
         child: Column(
+          // TODO: Center items on the card (103)
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             AspectRatio(
-              aspectRatio: 18.0 / 11.0,
-              child: Image.asset('assets/images/diamond.png'),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Title'),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Text('Secondary Text')
-                ],
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
               ),
-            )
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.headline6,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.subtitle2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
-      ),
-    );
-    return cards;
+      );
+    }).toList();
   }
 
   Widget build(BuildContext context) {
@@ -80,6 +107,6 @@ class Feed extends StatelessWidget {
             childAspectRatio: 8.0 / 9.0,
 
             //Tarjetas de ventas
-            children: _buildGridCards(10)));
+            children: _buildGridCards(context)));
   }
 }
